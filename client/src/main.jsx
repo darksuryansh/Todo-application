1a193cb9f4099c620269e6ac861ad05bd2eb8fb5
darksuryansh <suryanshbhardwaj04@gmail.com>
2025-12-27 23:43:51 +0530

    ready to deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'react-hot-toast';
-import { Analytics } from "@vercel/analytics/next"
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Toaster/>
-    <Analytics/>
+
   </StrictMode>,
 )
